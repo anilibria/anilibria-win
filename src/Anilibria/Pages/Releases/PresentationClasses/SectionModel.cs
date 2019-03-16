6269e09e802967fdf18d72fd635b27ed5fecf4d0
Romanus <trueromanus@gmail.com>
2019-03-16 12:18:04 +0300

    Added new files

---
+++ src/Anilibria/Pages/Releases/PresentationClasses/SectionModel.cs
@@ -0,0 +1,28 @@
+﻿namespace Anilibria.Pages.Releases.PresentationClasses {
+
+	/// <summary>
+	/// Section model.
+	/// </summary>
+	public class SectionModel {
+
+		/// <summary>
+		/// Title.
+		/// </summary>
+		public string Title
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Type.
+		/// </summary>
+		public SectionType Type
+		{
+			get;
+			set;
+		}
+
+	}
+
+}
---
+++ src/Anilibria/Pages/Releases/PresentationClasses/SectionType.cs
@@ -0,0 +1,24 @@
+﻿namespace Anilibria.Pages.Releases.PresentationClasses {
+
+	/// <summary>
+	/// Section type.
+	/// </summary>
+	public enum SectionType {
+
+		All,
+
+		Favorite,
+
+		NewReleases,
+
+		NewOnlineSeries,
+
+		NewTorrentSeries,
+
+		//Schedule,
+
+		//Seens,
+
+	};
+
+}
