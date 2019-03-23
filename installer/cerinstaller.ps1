98a8f7ae06c1ec5122aef100ff155ca68b608537
Romanus <trueromanus@gmail.com>
2019-03-23 12:43:47 +0300

    + Added logging + Added open true installer #20

--- installer/cerinstaller.ps1
+++ installer/cerinstaller.ps1
@@ -1,16 +1,33 @@
-#Import-Certificate -Filepath "$PSScriptRoot\test.cer" -CertStoreLocation Cert:\LocalMachine\Root
+Write-Host "Anilibria client installer for Windows 10"
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+Write-Host "Retrieve version current distributive...."
 
 $response = Invoke-WebRequest -Method Get -Uri "https://anilibria.github.io/anilibria-win/dist/Anilibria.appinstaller" -ContentType "application/xml" -TimeoutSec 60
 $xmlContent = [System.Text.Encoding]::UTF8.GetString($response.Content)
-$xmlClear = $xmlContent.Replace("﻿<?xml version=""1.0"" encoding=""utf-8""?>", "")
+$xmlClear = $xmlContent.Substring(39)
 $xml = [xml]$xmlClear
 $version = $xml.AppInstaller.Attributes["Version"].Value
 
-$pathToCetrificate = "https://anilibria.github.io/anilibria-win/dist/Anilibria_" + $version + "_Test/Anilibria_" + $version + "_x86_x64_arm.cer"
+Write-Host "Version $($version)" 
 
-$parentPath = [System.IO.Path]::GetTempPath() + "Anilibria_" + $version + "_x86_x64_arm.cer"
+$certificateUri = "https://anilibria.github.io/anilibria-win/dist/Anilibria_" + $version + "_Test/Anilibria_" + $version + "_x86_x64_arm.cer"
 
-Invoke-WebRequest -Uri $pathToCetrificate -Method Get -OutFile $parentPath
+$certificatePath = [System.IO.Path]::GetTempPath() + "Anilibria_" + $version + "_x86_x64_arm.cer"
 
-Import-Certificate -Filepath $parentPath -CertStoreLocation Cert:\LocalMachine\Root
+Write-Host "Retrieve certificate....."
 
+Invoke-WebRequest -Uri $certificateUri -Method Get -OutFile $certificatePath
+
+Write-Host "Import certificate....."
+
+Import-Certificate -Filepath $certificatePath -CertStoreLocation Cert:\LocalMachine\Root
+
+Write-Host "Remove certificate from temporary folder...."
+
+Remove-Item $certificatePath
+
+Write-Host "Installation complete"
+
+[System.Diagnostics.Process]::Start("ms-appinstaller:?source=https://anilibria.github.io/anilibria-win/dist/Anilibria.appinstaller")
