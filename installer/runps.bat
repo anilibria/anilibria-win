e97388390527ec6549372a0b8b45dd93c2ff578e
Romanus <trueromanus@gmail.com>
2019-04-16 20:37:35 +0300

    + Scripts for install application offline without dependencies (Windows Store, App Installer) #31

--- installer/runps.bat
+++ installer/runps.bat
@@ -1,3 +1,2 @@
 PowerShell -NoProfile -ExecutionPolicy Bypass -NoExit -File "%cd%\preinstall.ps1"
-PowerShell -NoProfile -ExecutionPolicy Bypass -NoExit -File "%cd%\Add-AppDevPackage.ps1" "-Force"
 exit
