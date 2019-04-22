7a60bf22522e2f389c45a63860fc8728c148046d
Romanus <trueromanus@gmail.com>
2019-04-22 20:16:11 +0300

    + added missing files

---
+++ src/Anilibria/Pages/Releases/PresentationClasses/TorrentDownloadMode.cs
@@ -0,0 +1,16 @@
+﻿namespace Anilibria.Pages.Releases.PresentationClasses {
+
+	/// <summary>
+	/// Torrent download mode.
+	/// </summary>
+	public enum TorrentDownloadMode {
+
+		OpenInTorrentClient = 1,
+
+		SaveAsFile = 2,
+
+		DownloadToDownloadManager = 3,
+
+	};
+
+}
---
+++ src/Anilibria/Pages/Releases/PresentationClasses/TorrentDownloadModeModel.cs
@@ -0,0 +1,28 @@
+﻿namespace Anilibria.Pages.Releases.PresentationClasses {
+
+	/// <summary>
+	/// Torrent download mode model.
+	/// </summary>
+	public class TorrentDownloadModeModel {
+
+		/// <summary>
+		/// Mode.
+		/// </summary>
+		public TorrentDownloadMode Mode
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Title.
+		/// </summary>
+		public string Title
+		{
+			get;
+			set;
+		}
+
+	}
+
+}
