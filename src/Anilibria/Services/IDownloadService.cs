57e23eff51f1eb7e399b762a00e7e6106b96c683
Romanus <trueromanus@gmail.com>
2019-05-06 20:11:10 +0300

    + Added interface for download service #4

---
+++ src/Anilibria/Services/IDownloadService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Anilibria.Services.PresentationClasses;
+
+namespace Anilibria.Services {
+
+	/// <summary>
+	/// Download service.
+	/// </summary>
+	public interface IDownloadService {
+
+		/// <summary>
+		/// Download file.
+		/// </summary>
+		/// <param name="uri">Uri file.</param>
+		/// <param name="id">Identifier.</param>
+		/// <param name="percentProgress">Percent progress.</param>
+		Task DownloadFile ( Uri uri , long id, Action<double> percentProgress );
+
+		/// <summary>
+		/// Get pending downloads.
+		/// </summary>
+		/// <returns></returns>
+		IEnumerable<DownloadItem> GetPendingDownloads ();
+
+		/// <summary>
+		/// Set download file.
+		/// </summary>
+		/// <param name="release">Release name.</param>
+		/// <param name="seria">Seria.</param>
+		/// <param name="uri">Uri.</param>
+		void AddDownloadFile ( DownloadItem downloadItem );
+
+		/// <summary>
+		/// Remove download file.
+		/// </summary>
+		/// <param name="id">Identifier.</param>
+		void RemoveDownloadFile ( long id );
+
+	}
+
+}
---
+++ src/Anilibria/Services/PresentationClasses/DownloadItem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Anilibria.Services.PresentationClasses {
+
+	/// <summary>
+	/// Download item.
+	/// </summary>
+	public class DownloadItem {
+
+		/// <summary>
+		/// Identifier.
+		/// </summary>
+		public long Id
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Release name.
+		/// </summary>
+		public string ReleaseName
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Серия.
+		/// </summary>
+		public string Seria
+		{
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Uri.
+		/// </summary>
+		public Uri Uri
+		{
+			get;
+			set;
+		}
+
+	}
+
+}
