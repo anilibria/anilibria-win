8be16d3507fcfbf781195d6d5c066ccde9f481f9
Romanus <trueromanus@gmail.com>
2019-05-26 10:41:03 +0300

    + Added missed file to source control

---
+++ src/Anilibria/Pages/Releases/ReleasesItems.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Anilibria.Pages.Releases.PresentationClasses;
+
+namespace Anilibria.Pages.Releases {
+
+	/// <summary>
+	/// Releases items for comboboxes and so on.
+	/// </summary>
+	public static class ReleasesItems {
+
+		public static ObservableCollection<FavoriteMarkItem> GetFavoriteMarkItems () {
+			var items = new List<FavoriteMarkItem> {
+				new FavoriteMarkItem {
+					Title = "Не используется",
+					Type = FavoriteMarkType.NotUsed
+				},
+				new FavoriteMarkItem {
+					Title = "В избранном",
+					Type = FavoriteMarkType.Favorited
+				},
+				new FavoriteMarkItem {
+					Title = "Не в избранном",
+					Type = FavoriteMarkType.NotFavorited
+				}
+			};
+			return new ObservableCollection<FavoriteMarkItem> ( items );
+		}
+
+		public static ObservableCollection<SeenMarkItem> GetSeenMarkItems () {
+			var items = new List<SeenMarkItem> {
+				new SeenMarkItem {
+					Title = "Не используется",
+					Type = SeenMarkType.NotUsed
+				},
+				new SeenMarkItem {
+					Title = "Просмотренные",
+					Type = SeenMarkType.Seen
+				},
+				new SeenMarkItem {
+					Title = "Посматриваемые",
+					Type = SeenMarkType.SeenNow
+				},
+				new SeenMarkItem {
+					Title = "Не просмотренные",
+					Type = SeenMarkType.NotSeen
+				}
+			};
+
+			return new ObservableCollection<SeenMarkItem> ( items );
+		}
+
+	}
+
+}
