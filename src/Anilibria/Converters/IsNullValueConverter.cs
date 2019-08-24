47aba2895589d541dac5ae2bdb8c5a7b7edde488
Romanus <trueromanus@gmail.com>
2019-08-24 11:10:51 +0300

    > Fix download item card

---
+++ src/Anilibria/Converters/IsNullValueConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace Anilibria.Converters {
+
+	/// <summary>
+	/// Is null value converter.
+	/// </summary>
+	public class IsNullValueConverter : IValueConverter {
+
+		public object Convert ( object value , Type targetType , object parameter , string language ) {
+			return value == null;
+		}
+
+		public object ConvertBack ( object value , Type targetType , object parameter , string language ) {
+			throw new NotImplementedException ();
+		}
+
+	}
+
+}
