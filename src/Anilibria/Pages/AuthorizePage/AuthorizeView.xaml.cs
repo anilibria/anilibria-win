4a995e1b1bc42ec124ce9e8cddde2aaacec1d2ef
Romanus <trueromanus@gmail.com>
2020-03-19 20:25:26 +0300

    + Added auth background

--- src/Anilibria/Pages/AuthorizePage/AuthorizeView.xaml.cs
+++ src/Anilibria/Pages/AuthorizePage/AuthorizeView.xaml.cs
@@ -1,5 +1,12 @@
-﻿using Anilibria.Services.Implementations;
+﻿using System;
+using Anilibria.Services.Implementations;
+using Windows.ApplicationModel;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace Anilibria.Pages.AuthorizePage {
 
@@ -14,6 +21,24 @@ namespace Anilibria.Pages.AuthorizePage {
 			DataContext = new AuthorizeViewModel ( ApiService.Current () , new AnalyticsService () );
 		}
 
+		private async void GridBackground_Loaded(object sender, RoutedEventArgs e)
+		{
+			var folder = await Package.Current.InstalledLocation.GetFolderAsync("Assets");
+			var file = await folder.GetFileAsync("authbackground.jpg");
+			using (var stream = await file.OpenAsync(FileAccessMode.Read))
+			{
+				var bitmapImage = new BitmapImage();
+				await bitmapImage.SetSourceAsync(stream);
+
+				var decoder = await BitmapDecoder.CreateAsync(stream);
+
+				GridBackground.Background = new ImageBrush
+				{
+
+					ImageSource = bitmapImage
+				};
+			}
+		}
 	}
 
 }
