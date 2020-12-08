949a3764c20113a42b6cbb4b4e3bf6c36b703235
Romanus <trueromanus@gmail.com>
2020-12-08 21:55:17 +0300

    + Added clear poster cache command

--- src/Anilibria/Pages/MaintenancePage/MaintenancePageViewModel.cs
+++ src/Anilibria/Pages/MaintenancePage/MaintenancePageViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using Anilibria.MVVM;
 using Anilibria.Services;
+using Anilibria.Services.Implementations;
 using System;
+using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace Anilibria.Pages.MaintenancePage
@@ -8,6 +10,10 @@ namespace Anilibria.Pages.MaintenancePage
 	public class MaintenancePageViewModel : ViewModel, INavigation
 	{
 		private readonly IAnalyticsService m_AnalyticsService;
+
+		private readonly object m_ClearPosterLock = new object();
+
+		private const string PosterGroupName = "Poster";
 
 		public MaintenancePageViewModel ( IAnalyticsService analyticsService ) {
 			m_AnalyticsService = analyticsService ?? throw new ArgumentNullException ( nameof ( analyticsService ) );
@@ -23,7 +29,20 @@ namespace Anilibria.Pages.MaintenancePage
 		private void OpenSidebar () => ShowSidebar?.Invoke ();
 
 		private void ClearPosterCache () {
+			var storageClient = StorageService.Current ();
+			var releasesService = ReleaseSingletonService.Current ();
 
+			Task.Run (
+				() => {
+					lock ( m_ClearPosterLock ) {
+						foreach ( var release in releasesService.GetReleases () ) {
+							if ( !storageClient.IsFileExists ( PosterGroupName , release.Id ) ) continue;
+
+							storageClient.DeleteFile ( PosterGroupName , release.Id );
+						}
+					}
+				}
+			);
 		}
 
 		public void NavigateFrom () {
